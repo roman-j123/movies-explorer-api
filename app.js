4c42d7246fbc7faa1b5a608599a3de70b8d2578b
Roman Ismagilov <roman.j123@ya.ru>
2021-07-07 13:56:33 +0300

    Добавил helmet

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const helmet = require('helmet');
 const mongoose = require('mongoose');
 
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 const app = express();
+app.use(helmet());
 
 mongoose.connect('mongodb://localhost:27017/bitfilmsdb', {
   useNewUrlParser: true,
