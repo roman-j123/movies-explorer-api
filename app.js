425c765ce8ee5d3d57b2c153b37e18d93f055722
Roman Ismagilov <roman.j123@ya.ru>
2021-07-07 13:52:33 +0300

    Добавил логирование

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
-const { PORT = 3000 } = process.env;
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
+const { PORT = 3000 } = process.env;
 const app = express();
+
 mongoose.connect('mongodb://localhost:27017/bitfilmsdb', {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: true,
 });
+
+app.use(requestLogger);
+// cookies
+// routes
+
+app.use(errorLogger);
+// errors
 
 app.listen(PORT, () => {
   console.log(`App listining on port: ${PORT}`);
